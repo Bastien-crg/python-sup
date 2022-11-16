7d844b8c218f086a370ae0f13066fdff5a30da64
Bastien Corgnac <76075825+Bastien-crg@users.noreply.github.com>
2022-11-16 14:32:55 +0100

    histogram

---
+++ histogram.py
@@ -0,0 +1,15 @@
+import csv
+import plotly.express as px
+import pandas as pd
+
+
+def main():
+    full_df = pd.read_csv("fr-esr-parcoursup.csv", sep=";")
+    filtered_df = full_df.loc[full_df["Capacité de l’établissement par formation"] <= 199]
+    fig = px.histogram(data_frame=filtered_df, x="Capacité de l’établissement par formation", nbins=20)
+
+    fig.show()
+
+
+if __name__ == '__main__':
+    main()
